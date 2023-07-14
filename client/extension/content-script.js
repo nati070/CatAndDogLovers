ed3635efbbe9bacd76f15c99a59fa90d9dabd05d
nati070 <nnati070@gmail.com>
2023-07-14 22:05:19 +0300

    fix msg send

--- client/extension/content-script.js
+++ client/extension/content-script.js
@@ -1,14 +1,13 @@
 const send = (async () => {
   try{   
     const response = await chrome.runtime.sendMessage({msg: "ping"});
-    return response
+    console.log(response)
   }
   catch{
     console.log("problem")
   }
 });
 
-
-setInterval(()=>send(),1000*60*3) //3 hours 
+setInterval(send,1000*60*3) //3 hours 
 
 
